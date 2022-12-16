8ea952cd2f680536429077f3c713d42af5190acf
GitHub Action <action@github.com>
2022-12-16 01:40:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/newrecord/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/newrecord/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.newrecord.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.newrecord.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/newrecord/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/newrecord/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.newrecord.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.newrecord.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
